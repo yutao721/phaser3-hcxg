6942ef200b4cf298ed30345fb8cfd8d0ebff544a
yutao <642231346@qq.com>
2023-09-21 20:06:48 +0800

    modify

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+# deploy.sh
+set -e
+
+# 打包
+npm run build
+
+# 进入目标文件夹
+cd dist
+
+# 提交到本地仓库
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# 提交到 你的项目的 gh-pages 分支
+git push -f git@github.com:yutao721/phaser3-hcxg.git master:gh-pages
+
+cd -
